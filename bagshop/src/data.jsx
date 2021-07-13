7493691cda6a08bbb9b670ecd5e46a695c630494
drsimplegraffiti <abayomiogunnusi@gmail.com>
2021-07-13 11:35:04 +0100

    initial commit

---
+++ bagshop/src/data.jsx
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Gucci',
+            price: 2000,
+            image: '/img/bag1.jpg',
+        },
+        {
+            id: '2',
+            name: 'Smith',
+            price: 5000,
+            image: '/img/bag2.jpg',
+        },
+        {
+            id: '3',
+            name: 'George',
+            price: 7000,
+            image: '/img/bag3.jpg',
+        },
+    ]
+}
+
+export default data;
